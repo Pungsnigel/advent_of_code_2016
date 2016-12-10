427d70dd5316d8d0364c6b2262fd0424317bcc88
Jesper Persson <jespert.persson@gmail.com>
2016-12-10 12:08:43 +0100

    remove ugly nils ¯\_(ツ)_/¯

--- 02/2/keypad.rb
+++ 02/2/keypad.rb
@@ -5,19 +5,19 @@ class Keypad
   attr_reader :active_number
 
   TRANSFORM_RULES = {
-    1   => { 'U' => nil, 'R' => nil, 'D' => 3,   'L' => nil },
-    2   => { 'U' => nil, 'R' => 3,   'D' => 6,   'L' => nil },
+    1   => { 'U' => 1,   'R' => 1,   'D' => 3,   'L' => 1   },
+    2   => { 'U' => 2,   'R' => 3,   'D' => 6,   'L' => 2   },
     3   => { 'U' => 1,   'R' => 4,   'D' => 7,   'L' => 2   },
-    4   => { 'U' => nil, 'R' => nil, 'D' => 8,   'L' => 3   },
-    5   => { 'U' => nil, 'R' => 6,   'D' => nil, 'L' => nil },
+    4   => { 'U' => 4,   'R' => 4,   'D' => 8,   'L' => 3   },
+    5   => { 'U' => 5,   'R' => 6,   'D' => 5,   'L' => 5   },
     6   => { 'U' => 2,   'R' => 7,   'D' => 'A', 'L' => 5   },
     7   => { 'U' => 3,   'R' => 8,   'D' => 'B', 'L' => 6   },
     8   => { 'U' => 4,   'R' => 9,   'D' => 'C', 'L' => 7   },
-    9   => { 'U' => nil, 'R' => nil, 'D' => nil, 'L' => 8   },
-    'A' => { 'U' => 6,   'R' => 'B', 'D' => nil, 'L' => nil },
+    9   => { 'U' => 9,   'R' => 9,   'D' => 9,   'L' => 8   },
+    'A' => { 'U' => 6,   'R' => 'B', 'D' => 'A', 'L' => 'A' },
     'B' => { 'U' => 7,   'R' => 'C', 'D' => 'D', 'L' => 'A' },
-    'C' => { 'U' => 8,   'R' => nil, 'D' => nil, 'L' => 'B' },
-    'D' => { 'U' => 'B', 'R' => nil, 'D' => nil, 'L' => nil }
+    'C' => { 'U' => 8,   'R' => 'C', 'D' => 'C', 'L' => 'B' },
+    'D' => { 'U' => 'B', 'R' => 'D', 'D' => 'D', 'L' => 'D' }
   }.freeze
 
   def initialize(start = 5)
@@ -29,7 +29,6 @@ class Keypad
   end
 
   def act(action)
-    return if TRANSFORM_RULES[@active_number][action].nil?
     @active_number = TRANSFORM_RULES[@active_number][action]
   end
 end
