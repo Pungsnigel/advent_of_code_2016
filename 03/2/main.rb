f81aa5183b778f74f75622731002d8c99d6b71f9
Jesper Persson <jespert.persson@gmail.com>
2016-12-15 16:55:04 +0100

    Cleanup 03/ - make use of transposing

--- 03/2/main.rb
+++ 03/2/main.rb
@@ -1,13 +1,5 @@
-valid_triangle = lambda do |side1, side2, side3|
-  ordered = [side1, side2, side3].sort
-  ordered[0] + ordered[1] > ordered[2]
-end
-sum = 0
-File.readlines('./input.txt').each_slice(3) do |sequence|
-  rows = sequence.map { |row| row.split(' ').map(&:to_i) }
-  sum += 1 if valid_triangle.call(rows[0][0], rows[1][0], rows[2][0])
-  sum += 1 if valid_triangle.call(rows[0][1], rows[1][1], rows[2][1])
-  sum += 1 if valid_triangle.call(rows[0][2], rows[1][2], rows[2][2])
-end
+triangles = (File.readlines('./input.txt').map do |row|
+  row.split(' ').map(&:to_i)
+end).transpose.flatten.each_slice(3).to_a
 
-puts sum
+puts triangles.map(&:sort).count { |sides| sides[0] + sides[1] > sides[2] }
